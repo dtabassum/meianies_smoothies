e3c6f30c0be6631be9e5f8748bfd1605c6a29eb9
dtabassum <daraksha.tabassum2001@gmail.com>
2024-07-05 14:41:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,9 +25,7 @@ pd_df=my_dataframe.to_pandas()
 #st.stop()
 
 ingredients_list = st.multiselect(
-    "Choose up to 5 ingredients:" 
-    , my_dataframe
-    , max_selections=5)
+    "Choose up to 5 ingredients:" , my_dataframe)
 
 
 if ingredients_list:
